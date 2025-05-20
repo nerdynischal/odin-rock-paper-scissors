fc758ad5f1ff9e0c92e9603d3914e301708b0d0f
nerdynischal <nerdynischal@gmail.com>
2025-05-20 13:51:48 +0100

    Create function to get human move

--- rps.js
+++ rps.js
@@ -1,9 +1,4 @@
-//Write Logic to get computer move
-//a random number from 0 to 1 from Math.random
-//if randomnumber less than 1/3 - rock
-//if random number less than 2/3 - paper
-//else - scissors
-
+//Generate a random computer move
 function getComputerChoice() {
   const randomNumber = Math.random();
   if (randomNumber < 1 / 3) {
@@ -17,3 +12,11 @@ function getComputerChoice() {
 
 const computerMove = getComputerChoice();
 console.log(computerMove);
+
+//Get player move
+function getHumanChoice() {
+  const humanMove = prompt("Whats your move?");
+  console.log(humanMove);
+}
+
+getHumanChoice();
